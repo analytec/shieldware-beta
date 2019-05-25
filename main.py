dd6ca8e1ad707ee48ecd6a03be465cc680a4748a
AaditT <aadit.trivedi03@gmail.com>
2019-05-25 13:56:07 -0700

    update

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def upload_file():
     return '''
     <!doctype html>
     <title>Upload an excel file</title>
-    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <h1>Excel file upload (.csv file)</h1>
     <form action="" method=post enctype=multipart/form-data>
     <p><input type=file name=file><input type=submit value=Upload>
     </form>
