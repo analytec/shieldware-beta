07c233375cc1445bc1c839d68cec601afdede024
Srihari Nanniyur <snanniyur@localhost.localdomain>
2019-05-25 18:54:47 -0700

    update

--- bulk_query.py
+++ bulk_query.py
@@ -1,40 +1,30 @@
-from multiprocessing import Process, Lock
+import multiprocessing as mp
 import twitterdata
 import engine
 import csv_utils
 import time
 
-lock = Lock()
+def twitter_query_wad(username):
+    all_tweets = twitterdata.get_all_tweets(username)
+    tweets_output = engine.getOutput(all_tweets)
+    weapon_vals = engine.checkWeapons(tweets_output)
+    alcohol_vals = engine.checkAlcohol(tweets_output)
+    drug_vals = engine.checkDrugs(tweets_output)
+    
+    print('Data acquired for user: ' + username)
+    return ({
+        'weapons' : weapon_vals,
+        'alcohol' : alcohol_vals,
+        'drugs'   : drug_vals
+    })
 
 # twitter_bulk_query_wad() - process and return the Weapons/Alcohol/Drugs content of multiple Twitter usernames
-def twitter_bulk_query_wad(user_list):
+def twitter_bulk_query_wad(user_list, threads=2):
     print(user_list)
-    results = {}
 
-    def twitter_query_wad(username):
-        all_tweets = twitterdata.get_all_tweets(username)
-        tweets_output = engine.getOutput(all_tweets)
-        weapon_vals = engine.checkWeapons(tweets_output)
-        alcohol_vals = engine.checkAlcohol(tweets_output)
-        drug_vals = engine.checkDrugs(tweets_output)
-        lock.acquire()
-        try:
-            results[username] = {
-                'weapons' : weapon_vals,
-                'alcohol' : alcohol_vals,
-                'drugs'   : drug_vals
-            }
-        finally:
-            lock.release()
-        print(results)
-        print('Results acquired for user ' + username)
+    pool = mp.Pool(threads)
+    results = pool.map(twitter_query_wad, user_list)
 
-    for element in user_list:
-        user_process = Process(target=twitter_query_wad, args = (element,))
-        user_process.start()
-    while len(results) < len(user_list):
-        print(str(len(results)) + ' results gathered so far.')
-        time.sleep(1)
     print('twitter_bulk_query_wad() completed.')
     return results
 
