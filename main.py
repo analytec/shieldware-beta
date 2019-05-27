c5f820cb4afcd2ca3939f89dd4071cc4dee425eb
AaditT <aadit.trivedi03@gmail.com>
2019-05-26 18:15:39 -0700

    a

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import *
 import twitterdata
-
-from engine import
+import engine
 from flask import Flask, request, jsonify
 import csv_utils
 from csv_utils import *
@@ -37,32 +36,34 @@ def dashboard_display():
             x_vals = x_vals
         )
 
-@app.route('/tw_graph', methods=['GET', 'POST'])
-def tw_graph():
+
+@app.route('/data', methods=['GET','POST'])
+def data():
     if request.method == 'POST':
-        '''username = str(request.form['username-tw'])
-        all_tweets = twitterdata.get_all_tweets(username)
-        tweets_output = engine.getOutput(all_tweets)
-        print("Getting drug vals...")
-        drug_vals = engine.checkDrugs(tweets_output)
-        print("Getting weapon vals...")
-        weapon_vals = engine.checkWeapons(tweets_output)
-        print("Getting alcohol vals...")
-        alcohol_vals = engine.checkAlcohol(tweets_output)'''
+        username = str(request.form['username-tw'])
+        all_tweets = twitterdata.get_all_tweets(username)#
         x_vals = engine.gen_list(10)
         username = str(request.form['username-tw'])
-        data = all_data[username]
-        return render_template(
-            'tw_graph.html',
-            name = username,
-            drug_vals = data['drugs'],
-            drug_max = max(data['drugs']),
-            weapon_vals = data['weapons'],
-            weapon_max = max(data['weapons']),
-            alcohol_vals = data['alcohol'],
-            alcohol_max = max(data['alcohol']),
-            x_vals = x_vals
-        )
+        data = engine.all_data[username]
+        return redirect(url_for('tw_graph',username=username)
+
+@app.route('/tw_graph', methods=['GET', 'POST'])
+def tw_graph(username):
+    #if request.method == 'POST':
+    username = str(request.form['username-tw'])
+    x_vals = engine.gen_list(10)
+    data = engine.all_data[username]
+    return render_template(
+        'tw_graph.html',
+        name = username,
+        drug_vals = data['drugs'],
+        drug_max = max(data['drugs']),
+        weapon_vals = data['weapons'],
+        weapon_max = max(data['weapons']),
+        alcohol_vals = data['alcohol'],
+        alcohol_max = max(data['alcohol']),
+        x_vals = x_vals
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8080)
