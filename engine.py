f44085babb153845835cc06a482a1f071b268a9b
Srihari Nanniyur <snanniyur@localhost.localdomain>
2019-05-26 17:15:47 -0700

    update

--- engine.py
+++ engine.py
@@ -1,3 +1,7 @@
+import multiprocessing as mp
+import twitterdata
+import csv_utils
+import pprint
 from sightengine.client import SightengineClient
 from creds import client_access, client_key
 client = SightengineClient(client_access, client_key)
@@ -37,3 +41,32 @@ def gen_list(count):
     for i in range(count):
         my_list.append(i)
     return my_list
+
+pp = pprint.PrettyPrinter(indent=4)
+
+def twitter_query_wad(username):
+    all_tweets = twitterdata.get_all_tweets(username)
+    tweets_output = getOutput(all_tweets)
+    weapon_vals = checkWeapons(tweets_output)
+    alcohol_vals = checkAlcohol(tweets_output)
+    drug_vals = checkDrugs(tweets_output)
+
+    print('Data acquired for user: ' + username)
+    return ({
+        'weapons' : weapon_vals,
+        'alcohol' : alcohol_vals,
+        'drugs'   : drug_vals
+    })
+
+# twitter_bulk_query_wad() - process and return the Weapons/Alcohol/Drugs content of multiple Twitter usernames
+# example usage: twitter_bulk_query_wad(['POTUS', 'narendramodi'], threads=4)
+def twitter_bulk_query_wad(user_list, threads=2):
+    print(user_list)
+
+    pool = mp.Pool(threads)
+    pool_results = pool.map(twitter_query_wad, user_list)
+    results = [{user_list[i] : pool_results[i]} for i in range(len(user_list))]
+
+    print('twitter_bulk_query_wad() completed.')
+    pp.pprint(results)
+    return results
